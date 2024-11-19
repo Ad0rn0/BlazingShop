78b721554b77e768d88937bf29cfb4d086ee05bb
Thiago Adorno | Xpert Pack <ad0rn0code@gmail.com>
2024-11-19 17:57:22 -0300

    #F0605: Router

--- obj/Debug/net7.0/BlazingShop.AssemblyInfo.cs
+++ obj/Debug/net7.0/BlazingShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+458b11ee070d8eec0bdd30623d9acb977140b403")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
